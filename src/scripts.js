a84f3026a913a5f1ef989c76fb282d78ea96dab9
mohammad <ronins1375@gmail.com>
2024-01-14 15:55:05 +0330

    css-fix2

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const mobileMenuButton = document.getElementById('mobile-menu-btn');
-  const mobileMenu = document.getElementById('mobile-menu');
-
-  mobileMenuButton.addEventListener('click', function () {
-    mobileMenu.classList.toggle('hidden');
-  });
-});
 
 //  humburger menu
 const Menu = document.querySelector('.menu');
