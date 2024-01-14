696e74061288341c8122bd0a787c54f13de8724d
mohammad <ronins1375@gmail.com>
2024-01-14 15:16:16 +0330

    huberger-menu

--- src/scripts.js
+++ src/scripts.js
@@ -6,3 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenu.classList.toggle('hidden');
   });
 });
+
+//  humburger menu
+const Menu = document.querySelector('.menu');
+const Burger = document.querySelector('.menu-burger');
+var bool = true;
+
+Burger.addEventListener('click', () => {
+  if (bool == true) {
+    Burger.classList.add('open');
+    Menu.style.right = 0;
+    bool = false;
+  } else {
+    Burger.classList.remove('open');
+    Menu.style.right = '-100%';
+    bool = true;
+  }
+})
